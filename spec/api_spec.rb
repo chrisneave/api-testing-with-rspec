5f0e7822f0dcb486e48ca9eaf509396cccc95355
Chris Neave <chrispneave@gmail.com>
2015-06-07 20:44:33 +0100

    Basic specs to verify the API
    Uses the HTTParty gem to make requests to the API and validate
    the responses.
    
    The subject method returns the response for a group with multiple
    it calls to verify each expectation of the response.

---
+++ spec/api_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+require "httparty"
+require "json"
+
+class Api
+  include HTTParty
+  base_uri "http://localhost:" + ENV["PORT"]
+end
+
+describe "API" do
+  describe "GET /ping" do
+    subject { Api.get("/ping") }
+
+    it "responds with a 200 status code" do
+      expect(subject.code).to equal(200)
+    end
+
+    it "responds with a 'pong'" do
+      expect(JSON.parse(subject.body)).to eql({"result" => "pong"})
+    end
+
+    it "responds with json" do
+      expect(subject.headers["Content-Type"]).to eql("application/json")
+    end
+  end
+
+  describe "GET /uri_that_does_not_exist" do
+    subject { Api.get("/uri_that_does_not_exist") }
+
+    it "responds with a 404 status code" do
+      expect(subject.code).to equal(404)
+    end
+  end
+end
